83a2ee2de33e50d4158522396a1d0fb303665e2f
jitendra sharma <jitendrasharma@jitendras-MacBook-Air.local>
2022-06-02 17:11:07 +0530

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSlides,
+  IonSlide,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+const slideOpts = {
+  initialSlide: 1,
+  speed: 400,
+};
 
 const Home: React.FC = () => {
   return (
@@ -16,7 +29,19 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        
+
+        <IonSlides pager={true} options={slideOpts}>
+          <IonSlide>
+            <h1>Slide 1</h1>
+          </IonSlide>
+          <IonSlide>
+            <h1>Slide 2</h1>
+          </IonSlide>
+          <IonSlide>
+            <h1>Slide 3</h1>
+          </IonSlide>
+        </IonSlides>
       </IonContent>
     </IonPage>
   );
